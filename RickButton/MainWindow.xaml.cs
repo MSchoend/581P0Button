2349064254c79a252983527450fc9a4393f54e4c
CarolineZhou <c_z.y@live.cn>
2020-02-06 22:32:00 -0700

    Update MainWindow.xaml.cs

--- RickButton/MainWindow.xaml.cs
+++ RickButton/MainWindow.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,6 +11,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
+using System.Windows.Media.Animation;
 using System.Windows.Shapes;
 
 namespace RickButton
@@ -20,9 +21,31 @@ namespace RickButton
     /// </summary>
     public partial class MainWindow : Window
     {
+        Storyboard meeseeksAnimation;
+        Storyboard garageExitAnimation;
+        private MediaPlayer mediaPlayer = new MediaPlayer();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            meeseeksAnimation = (Storyboard)Resources["MeeseeksAppears"];
+            garageExitAnimation = (Storyboard)Resources["GarageExit"];
+
+            meeseeksAnimation.Completed += (s, e) =>
+            {
+                mediaPlayer.Open(new Uri("..\\..\\sound\\meeseeks.mp3", UriKind.Relative));
+                mediaPlayer.Play();
+            };
+
+            Rick.MouseEnter += (s, e) =>
+            {
+                mediaPlayer.Open(new Uri("..\\..\\sound\\im_cool_rick.mp3", UriKind.Relative));
+                mediaPlayer.Play();
+            };
+            
         }
+
+        
     }
 }
